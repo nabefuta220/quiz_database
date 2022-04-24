ee01df18ef7329139bfabfae798d6cdef1384551
nabefuta <satoshinabe@220@gmail.com>
2022-04-24 10:29:33 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# quiz_database
+5I情報工学実験　Webアプリ開発演習(1巡目)
---
+++ quiz/models.py
@@ -0,0 +1,27 @@
+
+from pyexpat import model
+from tokenize import blank_re
+from django.db import models
+
+
+# Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=50)
+
+
+
+    parament_tag = models.ForeignKey(
+        'self', null=True, blank=True, related_name='parament',
+         on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    question_text=models.CharField(max_length=200)
+    answer_text=models.CharField(max_length=50)
+    tag = models.ForeignKey(Tag, null=True, blank=True,
+                           related_name='Tag', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.question_text
+
